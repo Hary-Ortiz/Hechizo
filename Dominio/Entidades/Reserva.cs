587d0047c6cb3b2f29e5bcbdf1a755981911e8e5
Hary Ortiz <harytortizt@gmail.com>
2026-03-03 18:30:08 -0500

    Creacion dentro de aplicación.

--- Dominio/Entidades/Reserva.cs
+++ Dominio/Entidades/Reserva.cs
@@ -10,22 +10,53 @@ namespace Dominio.Entidades
     /// .
     public class Reserva
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public int Id { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public DateTime Fecha { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public int NumeroPersonas { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public string? Observaciones { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public EstadoReserva Estado { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public int ClienteId { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public Cliente Cliente { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         protected Reserva() { }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fecha"></param>
+        /// <param name="numeroPersonas"></param>
+        /// <param name="observaciones"></param>
+        /// <param name="clienteId"></param>
         public Reserva(DateTime fecha, int numeroPersonas, string observaciones, int clienteId)
         {
             Fecha = fecha;
