afa764cb765a10eaba5340125be5c5e755ecf064
SANTIAGO MONROY <santiago-monroyq@unilibre.edu.co>
2026-03-03 22:38:48 -0500

    Actualizacion de capa Infraestructura

---
+++ Infraestructura/Data/HechizoDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace Infraestructura.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<DetallePedido> DetallesPedido { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Categoria>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Producto>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Cliente>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Pedido>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Reserva>()
+                .HasKey(r => r.Id);
+        }
+    }
+}
---
+++ Infraestructura/Repositorios/ClienteRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructura.Repositorios
+{
+    internal class ClienteRepository
+    {
+    }
+}
